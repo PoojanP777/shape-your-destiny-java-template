3364b5e09dae683f937e2dce37c77e2958ce8117
PoojanP777 <patelpoojan664@gmail.com>
2025-02-23 14:30:17 -0600

    shape your destiny
    Output of shape your destiny

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,136 @@
 package org.example;
 
+// Abstract base class
+abstract class Shape {
+    abstract double getArea();
+    abstract double getPerimeter();
+}
+
+// Circle class
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+// Rectangle class
+class Rectangle extends Shape {
+    protected double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double getArea() {
+        return length * width;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+// Right Triangle class
+class RightTriangle extends Shape {
+    protected double base, height;
+
+    public RightTriangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    double getArea() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    double getPerimeter() {
+        return base + height + Math.sqrt(base * base + height * height);
+    }
+}
+
+// Square class (inherits from Rectangle)
+class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+}
+
+// Isosceles Right Triangle class (inherits from RightTriangle)
+class IsoscelesRightTriangle extends RightTriangle {
+    public IsoscelesRightTriangle(double leg) {
+        super(leg, leg);
+    }
+}
+
+// Polygon interface
+interface Polygon {
+    int numberOfSides();
+}
+
+// Implementing Polygon interface in relevant classes
+class PolygonRectangle extends Rectangle implements Polygon {
+    public PolygonRectangle(double length, double width) {
+        super(length, width);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 4;
+    }
+}
+
+class PolygonSquare extends Square implements Polygon {
+    public PolygonSquare(double side) {
+        super(side);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 4;
+    }
+}
+
+class PolygonRightTriangle extends RightTriangle implements Polygon {
+    public PolygonRightTriangle(double base, double height) {
+        super(base, height);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 3;
+    }
+}
+
+class PolygonIsoscelesRightTriangle extends IsoscelesRightTriangle implements Polygon {
+    public PolygonIsoscelesRightTriangle(double leg) {
+        super(leg);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 3;
+    }
+}
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("You can write a driver program for your code here!");
-  }
+    public static void main(String[] args) {
+        System.out.println("You can write a driver program for your code here!");
+    }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,9 +4,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  // You can write your unit tests here.
-  @Test
-  void appWorks() {
-    assertEquals(true, true);
-  }
+
+    @Test
+    void testCircle() {
+        Circle c = new Circle(5);
+        assertEquals(Math.PI * 25, c.getArea(), 0.01);
+        assertEquals(2 * Math.PI * 5, c.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testRectangle() {
+        Rectangle r = new Rectangle(4, 5);
+        assertEquals(20, r.getArea(), 0.01);
+        assertEquals(18, r.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testRightTriangle() {
+        RightTriangle rt = new RightTriangle(3, 4);
+        assertEquals(6, rt.getArea(), 0.01);
+        assertEquals(12, rt.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testSquare() {
+        Square s = new Square(4);
+        assertEquals(16, s.getArea(), 0.01);
+        assertEquals(16, s.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testIsoscelesRightTriangle() {
+        IsoscelesRightTriangle irt = new IsoscelesRightTriangle(5);
+        assertEquals(12.5, irt.getArea(), 0.01);
+        assertEquals(5 + 5 + Math.sqrt(50), irt.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testPolygonImplementations() {
+        PolygonRectangle pr = new PolygonRectangle(4, 5);
+        assertEquals(4, pr.numberOfSides());
+
+        PolygonSquare ps = new PolygonSquare(4);
+        assertEquals(4, ps.numberOfSides());
+
+        PolygonRightTriangle prt = new PolygonRightTriangle(3, 4);
+        assertEquals(3, prt.numberOfSides());
+
+        PolygonIsoscelesRightTriangle pirt = new PolygonIsoscelesRightTriangle(5);
+        assertEquals(3, pirt.numberOfSides());
+    }
 }
+
